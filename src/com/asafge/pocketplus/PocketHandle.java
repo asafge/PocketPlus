d33a4b6029cfcdbf11d64808a0ce2b7ac77bbf7e
asafge <asafge@gmail.com>
2013-09-18 17:53:03 +0300

    Removed some spaces

--- src/com/asafge/pocketplus/PocketHandle.java
+++ src/com/asafge/pocketplus/PocketHandle.java
@@ -76,124 +76,106 @@ public class PocketHandle extends AccountHandle{
 		}
 	}
 	
-	private void failure(){
+	private void failure() {
 		dismiss();
 		failure(act, 401, "cancel");
 	}
 	
 	protected void auth() {
-
 		Task task = new Task();
 		task.execute();
 		
 	}
 	
-	private class Task extends AsyncTask<String, String, String> implements OnCancelListener, Runnable{
+	private class Task extends AsyncTask<String, String, String> implements OnCancelListener, Runnable {
 		
 		private AbstractAjaxCallback<?, ?> cb;
 		
 		@Override
 		protected String doInBackground(String... params) {
-	      
 			String url = null;
 			try {
 				url = provider.retrieveRequestToken(consumer, CALLBACK_URI);
-			}catch(Exception e){
+			}
+			catch(Exception e) {
 				AQUtility.report(e);				
 				return null;
 			} 
-			
 			return url;
 		}
 
-		
 		@Override
 		protected void onPostExecute(String url) {
 			
-			if(url != null){
-				
+			if(url != null) {
 				dialog = new WebDialog(act, url, new TwWebViewClient());		
 				dialog.setOnCancelListener(this);			
-				show();
-				//dialog.hide();
-				
+				show();		
 				dialog.load();
 				
-			}else{
+			} else {
 				failure();
 			}
-			
 		}
-
 
 		@Override
 		public void onCancel(DialogInterface arg0) {
 			failure();
 		}
 
-
 		@Override
 		public void run() {
 			auth(cb);
-		}
-	
-		
-	
+		}	
 	}
 	
-	public void authenticate(boolean refreshToken){
-		
-		if(!refreshToken && token != null && secret != null){
+	public void authenticate(boolean refreshToken) {
+		if (!refreshToken && token != null && secret != null) {
 			authenticated(secret, token);
-		}else{
+		}
+		else {
 			auth();
 		}
-		
 	}
 	
-	protected void authenticated(String secret, String token){
+	protected void authenticated(String secret, String token) {
 		
 	}
 	
 	private static final String POCKET_TOKEN = "aq.pocket.token";
 	private static final String POCKET_SECRET = "aq.pocket.secret";
 	
-	private String fetchToken(String key){
+	private String fetchToken(String key) {
 		return PreferenceManager.getDefaultSharedPreferences(act).getString(key, null);	
 	}
 	
-	private void storeToken(String key1, String token1, String key2, String token2){
+	private void storeToken(String key1, String token1, String key2, String token2) {
 		PreferenceManager.getDefaultSharedPreferences(act).edit().putString(key1, token1).putString(key2, token2).commit();	
 	}
 
-	private String extract(String url, String param){
-		
+	private String extract(String url, String param) {
 		Uri uri = Uri.parse(url);
 		String value = uri.getQueryParameter(param);
-		return value;
-		
+		return value;	
 	}
 
-	private class Task2 extends AsyncTask<String, String, String>{
+	private class Task2 extends AsyncTask<String, String, String> {
 
 		@Override
-		protected String doInBackground(String... params) {
-			
+		protected String doInBackground(String... params) {		
 			try {
 				provider.retrieveAccessToken(consumer, params[0]);
-			}catch(Exception e) {
+			} 
+			catch(Exception e) {
 				AQUtility.report(e);
 				return null;
-			} 
-			
+			}
 			return "";
 		}
 		
 		@Override
 		protected void onPostExecute(String url) {
-			
-			if(url != null){
-				
+			if(url != null){			
 				token = consumer.getToken();
 				secret = consumer.getTokenSecret();
 				
@@ -206,76 +188,58 @@ public class PocketHandle extends AccountHandle{
 				success(act);
 				
 				authenticated(secret, token);
-			}else{
-				
+			}
+			else {
 				failure();
 				authenticated(null, null);
 			}
-			
 		}
-		
-		
 	}
-	
 	
 	private class TwWebViewClient extends WebViewClient {
 		
-		
-		private boolean checkDone(String url){
+		private boolean checkDone(String url) {
 			if(url.startsWith(CALLBACK_URI)) {
-            	
-            	String verf = extract(url, "oauth_verifier");
-            	      
+            	String verf = extract(url, "oauth_verifier");           	      
             	dismiss();
             	Task2 task = new Task2();
             	task.execute(verf);
-            	
                 return true;
-            
-            }else if(url.startsWith(CANCEL_URI)) {               
+            }
+			else if(url.startsWith(CANCEL_URI)) {               
     			failure();
                 return true;
             }
-            
             return false;
 		}
 		
 		@Override
 		public boolean shouldOverrideUrlLoading(WebView view, String url) {
-			
             return checkDone(url);
 		}
 		
 		@Override
 		public void onPageStarted(WebView view, String url, Bitmap favicon) {
-			
-			AQUtility.debug("started", url);
-			
-			if(checkDone(url)){
-			}else{
+			AQUtility.debug("started", url);		
+			if(checkDone(url)) {
+			}
+			else {
 				super.onPageStarted(view, url, favicon);
 			}
-			
 		}
 		
 		@Override
 		public void onPageFinished(WebView view, String url) {
 			AQUtility.debug("finished", url);
 			super.onPageFinished(view, url);
-			
 			show();
 		}
-		
 		
 		@Override
 		public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
 			failure();
 		}
-		
-		
 	}
-
-	
 
 	@Override
 	public boolean expired(AbstractAjaxCallback<?, ?> cb, AjaxStatus status) {
@@ -284,49 +248,42 @@ public class PocketHandle extends AccountHandle{
 	}
 
 	@Override
-	public boolean reauth(final AbstractAjaxCallback<?, ?> cb) {
-		
+	public boolean reauth(final AbstractAjaxCallback<?, ?> cb) {		
 		token = null;
 		secret = null;
 		storeToken(POCKET_TOKEN, null, POCKET_SECRET, null);
 		
 		Task task = new Task();
 		task.cb = cb;
-		
 		AQUtility.post(cb);
-		
 		return false;
 	}
 
 	@Override
 	public void applyToken(AbstractAjaxCallback<?, ?> cb, HttpRequest request) {
-	
 		AQUtility.debug("apply token", cb.getUrl());
-		
-		try{
+		try {
 			consumer.sign(request);
-		}catch(Exception e){
-			AQUtility.report(e);
 		} 
-		
+		catch(Exception e) {
+			AQUtility.report(e);
+		}
 	}
 	
 	@Override	
-	public void applyToken(AbstractAjaxCallback<?, ?> cb, HttpURLConnection conn){
-		
+	public void applyToken(AbstractAjaxCallback<?, ?> cb, HttpURLConnection conn) {
 		AQUtility.debug("apply token multipart", cb.getUrl());
 		
 		OAuthConsumer oac = new DefaultOAuthConsumer(consumer.getConsumerKey(), consumer.getConsumerSecret());
 		oac.setTokenWithSecret(consumer.getToken(), consumer.getTokenSecret());
 		
-		try{
+		try {
 			oac.sign(conn);
-		}catch(Exception e){
-			AQUtility.report(e);
 		} 
-		
+		catch(Exception e) {
+			AQUtility.report(e);
+		}
 	}
-
 
 	@Override
 	public boolean authenticated() {
@@ -334,15 +291,11 @@ public class PocketHandle extends AccountHandle{
 	}
 
 	@Override
-	public void unauth(){
-		
+	public void unauth() {
 		token = null;
 		secret = null;
-		
 		CookieSyncManager.createInstance(act);
 		CookieManager.getInstance().removeAllCookie();		
-		
 		storeToken(POCKET_TOKEN, null, POCKET_SECRET, null);
 	}
-	
 }
